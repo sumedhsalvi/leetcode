ef114c0082c3c603322cb2620570e9aa5858cf37
Sumedh Salvi <sumedhsalvi25@gmail.com>
2022-08-17 23:28:08 -0400

    Time: 1809 ms (51.92%), Space: 25.5 MB (23.81%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,15 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        answer = [0] * n
-        stack = []
+        res = [0] * len(temperatures)
+        stack = [] # pair: [temp, index]
         
-        for curr_day, curr_temp in enumerate(temperatures):
-            # Pop until the current day's temperature is not
-            # warmer than the temperature at the top of the stack
-            while stack and temperatures[stack[-1]] < curr_temp:
-                prev_day = stack.pop()
-                answer[prev_day] = curr_day - prev_day
-            stack.append(curr_day)
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i - stackInd)
+            stack.append([t, i])
+        return res
+                
         
-        return answer
+        
